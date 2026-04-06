b7c8a6b59de775eaa6b78b7018d32e2070c00f40
tvr3 <tvr3@illinois.edu>
2026-04-06 16:11:28 -0500

    Adding Stack and Queue Implementation

---
+++ CIS122Spr26ArraysBigOExamples/QueueImplementation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIS122Spr26ArraysBigOExamples
+{
+    public class QueueImplementation
+    {
+        private LinkedList<int> _values = new LinkedList<int>();
+
+        public void Enqueue(int value)
+        {
+            _values.AddLast(value);
+        }
+
+
+        public int Dequeue()
+        {
+            var temp = _values.First.Value;
+            _values.RemoveFirst();
+            return temp;
+        }
+
+
+        public int Peek()
+        {
+            return _values.First.Value;
+        }
+    }
+}
---
+++ CIS122Spr26ArraysBigOExamples/StackImplementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIS122Spr26ArraysBigOExamples
+{
+    public class StackImplementation
+    {
+        private LinkedList<int> _values  = new LinkedList<int>();
+
+        public void Push(int value)
+        {
+            _values.AddFirst(value);
+        }
+
+        public int Peek()
+        {
+            return _values.First.Value;
+        }
+
+        public int Pop()
+        {
+            var temp = _values.First.Value;
+            _values.RemoveFirst();
+            return temp;
+        }
+    }
+}
